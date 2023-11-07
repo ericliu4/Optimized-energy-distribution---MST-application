218decefe8408bcc842b0ae2135f05f49bbb0af0
Eric Liu <eric7liu562@gmail.com>
2023-11-06 21:09:02 -0500

    load data for all 3 csv files (err handling later)

--- codeFiles/dataLoader.py
+++ codeFiles/dataLoader.py
@@ -1,14 +1,31 @@
 import pandas as pd
+'''
+file used to import data from all 3 CSV files
+return list of lists back to simulate function
 
+ObstacleLoader uses a riemann sum technique to 
+add the difference in cost variable when going over 
+high/low altitudes
+'''
 def nodesLoader(name):
-
-    print(2)
-    return 2
+    data = pd.read_csv(name)
+    #print(data)
+    lst = data[['XLocation', 'YLocation', 'ZLocation']].values.tolist()
+    print(lst)
+    return lst
 
 def sourceLoader(name):
-    print(3)
-    return 3
+    data = pd.read_csv(name)
+    #print(data)
+    lst = data[['XLocation', 'YLocation', 'ZLocation']].values.tolist()
+    print(lst)
+    return lst
 
+#add error checking function later (make sure the corners are correct) 
+#aim to have an adjust function to be implemented later
 def obstacleLoader(name):
-    print(4)
-    return 4
+    ##TopLeftX,TopLeftY,BottomRightX,BottomRightY,Height
+    data = pd.read_csv(name)
+    lst = data[['TopLeftX','TopLeftY', 'BottomRightX', 'BottomRightY', 'Height']].values.tolist()
+    print(lst)
+    return lst
