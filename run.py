9e63730627598928ee4ba906bbfdb9bc4ae15cfd
Eric Liu <eric7liu562@gmail.com>
2023-11-06 21:09:21 -0500

    adjusted csv file path

--- run.py
+++ run.py
@@ -6,9 +6,9 @@ print(sys.executable)
 
 
 algorithmVersion = 1
-inputDataFile = '../inputDataFiles/inputDataNodes - Sheet1.csv'
-inputSourceFile = '../inputDataFiles/sourceNodes - Sheet1.csv'
-inputObstacleFile = '../inputDataFiles/obstacleData - Sheet1.csv'
+inputDataFile = 'inputDataFiles/InputDataNodes - Sheet1.csv'
+inputSourceFile = 'inputDataFiles/sourceNodes - Sheet1.csv'
+inputObstacleFile = 'inputDataFiles/obstacleData - Sheet1.csv'
 instance1 = graph.simulate(algorithmVersion, inputDataFile, inputSourceFile, inputObstacleFile)
 print(instance1.testFile())
 
